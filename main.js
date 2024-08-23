ee0126ffa37c4bc2272c82641450b5156976e373
Muhammad Jazib Qamar <jazibqamar54@gmail.com>
2024-08-23 15:34:12 +0800

    Initial commit

--- main.js
+++ main.js
@@ -12,29 +12,14 @@ function loadSkillsPage() {
   var skills = [
     { name: "C", image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" },
     { name: "C++", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" },
-    { name: "Java", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" },
     { name: "Python", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
-    { name: "HTML", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
-    { name: "CSS", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
-    { name: "JavaScript", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" },
-    { name: "ReactJs", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-    { name: "AngularJs", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg" },
     { name: "NodeJs", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" },
-    { name: "ExpressJs", image: "./assets/express.png" },
-    { name: "MongoDB", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" },
+    
     { name: "Matlab", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/matlab/matlab-original.svg" },
     { name: "Git", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" },
     { name: "GitHub", image: "./assets/github.png" },
     { name: "Discord.js", image: "./assets/discord.png" },
     { name: "VS Code", image: "./assets/vs_code.svg" },
-    { name: "Manjaro", image: "https://user-images.githubusercontent.com/63140632/145705669-2050df14-6b64-44a9-aa66-dd62bafb4936.png" },
-    { name: "NextJs", image: "https://user-images.githubusercontent.com/63140632/159057943-66bda3ce-5f77-41e7-814e-fc57f6e3540a.svg" },
-    { name: "SASS", image: "https://user-images.githubusercontent.com/63140632/159058821-9e2c882c-b0cd-4248-9ec5-5f87c32e4f3c.svg" },
-    { name: "JQuery", image: "https://user-images.githubusercontent.com/63140632/138343999-b14291bb-a436-47c8-8b6e-8e27843f2228.png" },
-    { name: "Tailwind", image: "https://user-images.githubusercontent.com/63140632/155379705-ba343eb1-9336-49ed-8cb2-14e476320433.svg" },
-    { name: "MySQL", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" },
-    { name: "IntelliJ", image: "https://user-images.githubusercontent.com/63140632/159059401-30767299-0aff-471b-a4d5-f0cf1b740902.svg" },
-    { name: "Figma", image: "https://user-images.githubusercontent.com/63140632/159059723-a6c20188-ea00-45b0-a930-fbcf1a4e804d.svg" },
   ];
 
   var allSkills = document.getElementById("all-skills");
@@ -53,75 +38,52 @@ function loadSkillsPage() {
 function loadProjectsPage() {
   projects = [
     {
-      title: "ADG-VIT Official Website",
-      image: "./assets/adg-official.png",
+      title: "I-IMCS:IoT Based Industrial Monitoring and Control System",
+      image: "./assets/rp.png",
       description:
-        "The official website of Apple Developers Group, VIT Technical Club. Built using ReactJs and various npm packages.",
-      deploy: "https://adgvit.com",
-      repo: "https://github.com/ADG-VIT/ADG-VIT-Website",
-    },
-    {
-      title: "DC Official Website",
-      image: "./assets/dc-official.png",
-      description:
-        "Contributing to and maintaining the official website for Design and Code built using ReactJs.",
+        "HINET based HMI Interface developed for real time monitoring of Industrial Utilities leverageing MQTT protocol",
       deploy: "",
-      repo: "https://github.com/csivitu/CCS-Frontend-2022",
+      repo: "",
     },
     {
-      title: "Core Committee Selections",
-      image: "./assets/ccs-csivit.png",
+      title: "I-AIEUMS:IoT Based Advanced Industrial Energy Utility Management System",
+      image: "./assets/rp.png",
       description:
-        "The official website for Core Committee Selections for CSI-VIT built using MERN stack.",
+        "RS-485 Modbus Based Industrial Energy Consuming Utilities was monitored utilized Digital Twin tech",
       deploy: "",
-      repo: "https://github.com/csivitu/CCS-Frontend-2022",
+      repo: "",
     },
     {
-      title: "Otôbus",
-      image: "./assets/otobus.png",
+      title: "I-AMS:IoT Based Air Pollution Monitoring System",
+      image: "./assets/rp.png",
       description:
-        "A MEAN Stack project for Bus Management System having features like view, book and cancel buses.",
-      deploy: "https://designandcode.us",
-      repo: "https://github.com/Design-and-Code/Design-and-Code",
-    },
-    {
-      title: "Riddler Frontend 2021",
-      image: "./assets/riddler.png",
-      description:
-        "Frontend of Riddler game website made using ReactJs framework and various npm packages.",
-      deploy: "https://riddler-sample.vercel.app/",
-      repo: "https://github.com/csivitu/riddler-frontend-2021",
-    },
-    {
-      title: "LaserTag Frontend 2021",
-      image: "./assets/lasertag.png",
-      description:
-        "Frontend of LaserTag Slot Booking Portal made using ReactJs framework.",
+        "MQ-135 ,PMS 5003 Partculate Sensors were interfaced with ESP-32 Microncontroller monitoring AQI realtime on Thinks Speak platform",
       deploy: "",
-      repo: "https://github.com/csivitu/LaserTag-Frontend",
+      repo: "",
     },
     {
-      title: "CMS 2021",
-      image: "./assets/cms.png",
+      title: "IoT Based Voice Controlled Mobile Robots ",
+      image: "./assets/rp.png",
       description:
-        "A contact management system built using MySQL and JavaFX.",
+        "Voice Recognition based  Mobile Robots were controlled.Programed using python language.MIT App inventor was utlizied to make GUI",
       deploy: "",
-      repo: "https://github.com/nimishjn/contact-management-system-mysql-javafx",
+      repo: "",
     },
     {
-      title: "Portfolio Website",
-      image: "./assets/portfolio.png",
+      title: "FIR Filter Implementation on SPARTAN 3E Fpga",
+      image: "./assets/rp.png",
       description:
-        "Portfolio website to display my skills, projects, research work, etc built using HTML, CSS and JavaScript.",
-      deploy: "https://www.nimish-jain.com/",
-      repo: "https://github.com/nimishjn/portfolio/",
+        "Finite Impulse Response Filter was designed on XILINX and then somehow tried to implent on SPARTAN 3E😊",
+      deploy: "",
+      repo: "",
     },
     {
-      title: "Links",
-      image: "./assets/links.png",
-      description: "Used to host all my skills and links in one website.",
-      deploy: "https://links.nimish-jain.com/",
-      repo: "https://github.com/nimishjn/links/",
+      title: "UAV Recognition using YOLOv",
+      image: "./assets/rp.png",
+      description:
+        "Framework was developed to train state of the start YOLO algorithm to detect Unamanned Aerial Vehicle",
+      deploy: "",
+      repo: "",
     },
   ];
 
@@ -156,27 +118,17 @@ function loadResearchPage() {
     {
       name: "ORCID iD",
       logo: "<i class='fab fa-orcid'></i>",
-      link: "https://orcid.org/0000-0001-9607-0764",
+      link: "https://orcid.org/my-orcid?orcid=0009-0007-5946-2508",
     },
     {
       name: "Google Scholar",
       logo: "<i class='fas fa-graduation-cap'></i>",
-      link: "https://scholar.google.com/citations?user=oKBKRQ0AAAAJ&hl=en",
+      link: "https://scholar.google.com/citations?user=0JPd0FEAAAAJ&hl=zh-TW&oi=ao",
     },
     {
       name: "ResearchGate",
       logo: "<i class='fab fa-researchgate'></i>",
-      link: "https://www.researchgate.net/profile/Nimish-Jain-4",
-    },
-    {
-      name: "Publons",
-      logo: "<i class='fas fa-file-powerpoint'></i>",
-      link: "https://publons.com/researcher/4167049/nimish-jain/",
-    },
-    {
-      name: "SciProfiles",
-      logo: "<i class='fas fa-graduation-cap'></i>",
-      link: "https://sciprofiles.com/profile/1689275",
+      link: "https://www.researchgate.net/profile/Muhammad-Jazib-3",
     },
   ];
 
@@ -200,20 +152,27 @@ function loadResearchPage() {
   researchPapers = [
     {
       title:
-        "Modeling of Predictable Variations in Multi-Time Instant Ambient Temperature Time Series",
-      link: "https://ieeexplore.ieee.org/document/9404497",
+        "MIND TWIN AI:Multiphysics Informed Digital Twin for fault localization in Induction Motor using AI (Published IEEE BdKCSE'23 Italy)",
+
+      link: "",
       logo: "<i class='fas fa-file-alt'></i>",
     },
     {
       title:
-        "Performance Comparison of Two Statistical Parametric Methods for Outlier Detection and Correction",
-      link: "https://www.sciencedirect.com/science/article/pii/S2405896321014919",
+        "Deep Learning Resource Allocation in Radio Centeric Netwroks (Accepted IEEE ICECE'23 China) ",
+      link: "",
       logo: "<i class='fas fa-file-alt'></i>",
     },
     {
       title:
-        "New Performance Evaluation Metrics for Outlier Detection and Correction",
-      link: "https://link.springer.com/chapter/10.1007/978-981-16-9033-4_63",
+        "Vision Transformer based Semantic Communication for Next Gen Wireless Networks (Submitted IEEE WCNC'24 Italy)",
+      link: "",
+      logo: "<i class='fas fa-file-alt'></i>",
+    },
+    {
+      title:
+        "Reinforcement Learning based Power Allocation in UAV Assisted Aerial Active RIS for COMP-NOMA Networks (In-Progress)",
+      link: "",
       logo: "<i class='fas fa-file-alt'></i>",
     },
   ];
